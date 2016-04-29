da01aa9f10e3b77e1381018c4f3c2ec2012bbef3
Arsen Mamikonyan <arsen@mamikonyan.am>
2016-04-29 14:06:06 -0400

    Adjust python wrapper to new pipelines

--- script/py_analysis/plot_score_contours.py
+++ script/py_analysis/plot_score_contours.py
@@ -1,10 +1,12 @@
 import json
 import math
+import os
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
 import argparse
+
+SAVEFIG_INFER_VALUE = 'INFER_SAVEFIG_FILENAME'
 
 
 def parse_args(*argument_list):
@@ -16,6 +18,7 @@ def parse_args(*argument_list):
                       default='target/scores/3gaussians-7k-grid.json')
   parser.add_argument('--plot', default='density',
                       choices=['density', 'components', 'difference'])
+  parser.add_argument('--savefig', nargs='?', const=SAVEFIG_INFER_VALUE)
   args = parser.parse_args(*argument_list)
   return args
 
@@ -28,7 +31,7 @@ def load_json_dump(filename):
     x, y = point['metrics']['data']
     X.append(x)
     Y.append(y)
-    Z.append(point['density'])
+    Z.append(point['score'])
   return X, Y, Z
 
 
@@ -37,11 +40,8 @@ def load_cluster_parameters(filename):
     data = json.load(infile)
   return [p['data'] for p in data]
 
-if __name__ == '__main__':
-  # XX, YY, ZZ = load_json_dump('target/scores/3gaussians-7k-data.json')
-  # plt.hist2d(XX, YY, bins=60)
-  args = parse_args()
 
+def plot_score_contours(args):
   X, Y, Z = load_json_dump(args.scored_grid)
 
   minX, maxX = min(X), max(X)
@@ -52,18 +52,16 @@ if __name__ == '__main__':
   Y = np.reshape(Y, (size, size))
   Z = np.reshape(Z, (size, size))
 
-  means = load_cluster_parameters('target/scores/{0.test_class}-{0.test_method}-means.json'.format(args))  # noqa
-  sigmas = load_cluster_parameters('target/scores/{0.test_class}-{0.test_method}-covariances.json'.format(args))  # noqa
-  with open('target/scores/{0.test_class}-{0.test_method}-weights.json'.format(args)) as infile:  # noqa
-    weights = json.load(infile)
+  try:
+    means = load_cluster_parameters('target/scores/{0.test_class}-{0.test_method}-means.json'.format(args))  # noqa
+    sigmas = load_cluster_parameters('target/scores/{0.test_class}-{0.test_method}-covariances.json'.format(args))  # noqa
+    with open('target/scores/{0.test_class}-{0.test_method}-weights.json'.format(args)) as infile:  # noqa
+      weights = json.load(infile)
 
-  for i in range(len(means)):
-    print weights[i], means[i], sigmas[i]
-
-  # normalize weights to sum to 1.
-  weights = [w / sum(weights) for w in weights]
-
-  print weights
+    # normalize weights to sum to 1.
+    weights = [w / sum(weights) for w in weights]
+  except:
+    pass
 
   def format_args(i):
     kwargs = {}
@@ -74,7 +72,6 @@ if __name__ == '__main__':
     kwargs['sigmaxy'] = sigmas[i][0][1]
     return kwargs
 
-  normals = []
   Zgaussians = weights[0] * mlab.bivariate_normal(X, Y, **format_args(0))
   for i in range(1, len(means)):
     Zgaussians += weights[i] * mlab.bivariate_normal(X, Y, **format_args(i))
@@ -87,4 +84,20 @@ if __name__ == '__main__':
     CS = plt.contour(X, Y, Z - Zgaussians, linewidth=10000, inline=1)
   plt.clabel(CS, inline=1)
 
-  plt.show()
+  if args.savefig:
+    if args.savefig == SAVEFIG_INFER_VALUE:
+      name = os.path.basename(args.scored_grid).rsplit('.')[0]
+      filename = 'target/plots/{}.png'.format(name)
+    else:
+      filename = args.savefig
+    print 'saving figure to - ', filename
+    plt.savefig(filename, dpi=320)
+  else:
+    plt.show()
+
+
+if __name__ == '__main__':
+  # XX, YY, ZZ = load_json_dump('target/scores/3gaussians-7k-data.json')
+  # plt.hist2d(XX, YY, bins=60)
+  args = parse_args()
+  plot_score_contours(args)
